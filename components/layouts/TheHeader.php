29e7d1f072f136c88ed2b018103075396497caaf
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-02-23 17:20:32 +0200

    finish landing page

--- components/layouts/TheHeader.php
+++ components/layouts/TheHeader.php
@@ -12,6 +12,10 @@
   <link rel="stylesheet" href="assets/css/all.min.css">
   <!-- END:: INCLUDING FONT AWESOME STYLE FILE -->
 
+  <!-- START:: INCLUDING GOOGLE FONTS -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400&display=swap">
+  <!-- END:: INCLUDING GOOGLE FONTS -->
+
   <!-- START:: INCLUDING MAIN STYLE FILE -->
   <link rel="stylesheet" href="assets/css/style.css">
   <!-- END:: INCLUDING MAIN STYLE FILE -->
--- index.php
+++ index.php
@@ -3,7 +3,40 @@
 <!-- END:: INCLUDING THE HEADER -->
 
 <div class="page-content">
-  
+  <div class="landing-card">
+    <!-- START:: UPPER BAR -->
+      <div class="upper-bar">
+        <img src="assets/pics/logo.png">
+      </div>
+    <!-- START:: UPPER BAR -->
+
+    <!-- START:: LANDING CARD CONTENT -->
+      <div class="container">
+        <div class="row align-items-center justify-content-center">
+          <div class="landing-pic col-12 col-md-6">
+            <img src="assets/pics/landing-pic.svg" alt="Landing Page">
+          </div>
+
+          <div class="landing-text col-12 col-md-6">
+            <h2 class="text-center my-3"> Get Our Mobile App Now </h2>
+            <p class="lead mb-3"> 
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Minima praesentium reiciendis aspernatur pariatur, magni quis nemo laborum. 
+              Eveniet possimus saepe dolor. 
+            </p>
+            <div class="row">
+              <div class="col-12 col-md-6">
+                <img src="assets/pics/google-play.png">
+              </div>
+              <div class="col-12 col-md-6">
+                <img src="assets/pics/app-store.png">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- END:: LANDING CARD CONTENT -->
+  </div>
 </div>
 
 <!-- START:: INCLUDING THE FOOTER -->
